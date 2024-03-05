cb4bed9e671d925d523212a3c0ef4cd32e74706a
Aman Jain <88729130+aman12121121@users.noreply.github.com>
2024-03-05 23:18:04 +0530

    Time: 25 ms (55.19%) | Memory: 23.8 MB (82.96%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -3,22 +3,20 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         vector<int> arr(2,0);
         int n=nums.size();
-        int org=(n*(n+1))/2;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
+        int org;
         for(int i=0;i<n;i++){
             if(nums[abs(nums[i])-1]<0){
                 arr[0]=abs(nums[i]);
-                break;
             }else{
                 nums[abs(nums[i])-1]*=-1;
             }
         }
-        
-        sum-=arr[0];
-        arr[1]=org-sum;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                arr[1]=i+1;
+                break;
+            }
+        }
         return arr;
         
 
