01aa3b681f108109c91c9ae09f030c92b0f2cb4c
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-03-09 20:46:35 +0530

    Time: 4 ms (68.48%) | Memory: 12.6 MB (45.52%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -5,13 +5,22 @@ public:
         set<int>st;
         int n=nums1.size();
         int m=nums2.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(nums1[i]==nums2[j]){
-                    st.insert(nums1[i]);
-                }
+        int i,j;
+        i=j=0;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        while(i<n && j<m){
+            if(nums1[i]==nums2[j]){
+                st.insert(nums1[i]);
+                i++;j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
             }
         }
+        
         for(auto value:st){
             ans.push_back(value);
         }
