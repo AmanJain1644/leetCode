3d6655e9f397ede9c2b00057c6c7f64e01694586
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-03-09 12:57:13 +0530

    Time: 62 ms (89.16%) | Memory: 52.9 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -5,6 +5,9 @@ public:
         i=j=0;
         int n=nums1.size();
         int m=nums2.size();
+        if(nums1[n-1]<nums2[0] || nums2[m-1]<nums1[0]){
+            return -1;
+        }
         while(i<n && j<m){
             if(nums1[i]==nums2[j]){
                 return nums1[i];
