993ab6527d74ceee802c2c6f53f49a0d70531e97
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-03-10 14:01:46 +0530

    Time: 6 ms (30.35%) | Memory: 11.2 MB (39.30%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans(n,0);
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1] && nums[i]!=0){
-                nums[i]*=2;
-                nums[i+1]=0;
+        int cauntZero = 0;
+        for (int i = 0,j=1; i < nums.size()-1; i++,j++) {
+            if (nums[i] == nums[j]) {
+                nums[i] = nums[i] + nums[j];
+                nums[j] = 0;
+            }      
+        }
+        for(int i=0;i<nums.size();i++){
+            if (nums[i] == 0) {
+                cauntZero++;
+            }
+            else {
+                int num = nums[i];
+                nums[i] = 0;
+                nums[i - cauntZero] = num;
             }
         }
-        int i=0;
-        for(auto value:nums){
-            if(value){
-                ans[i]=value;
-                i++;
-            }
-        }
-        return ans;
-        
+        return nums;
     }
 };
