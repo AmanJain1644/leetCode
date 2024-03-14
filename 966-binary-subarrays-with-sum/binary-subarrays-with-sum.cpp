6a07078f4dd65d48d404bdf653617936f5577600
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-03-14 19:55:42 +0530

    Time: 15 ms (96.22%) | Memory: 31 MB (85.39%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int i{0},j{0},count{0},sum{0};
-        int n=nums.size();
-        while (j<n){
-            sum+=nums[j];
-            while(sum>goal && i<j){
-                sum-=nums[i];
-                i++;
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
+        return atmost_subarr_with_goal(nums, goal) - atmost_subarr_with_goal(nums, goal - 1);
+    }
+    int atmost_subarr_with_goal(vector<int> &nums, int goal)
+    {
+        if (goal < 0)
+            return 0;
+        int sum = 0, i = 0, count = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            sum += nums[j];
+            while (sum > goal)
+            {
+                sum -= nums[i++];
             }
-            if(sum==goal){
-                int k=i;
-                while(k<j && nums[k]==0){
-                    count++;
-                    k++;
-                }
-                count++;
-            }
-            j++;
+            count += (j - i + 1);
         }
         return count;
-        
     }
 };
