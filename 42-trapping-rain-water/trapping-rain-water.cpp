c80c9bc9aaaef35a08a7288bfda7af2bdbca0036
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-03-18 21:22:32 +0530

    Time: 11 ms (54.82%) | Memory: 23.2 MB (24.79%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,10 +4,10 @@ public:
         int n=height.size();
         int water_trapped=0;
 
-        vector<int>left_max(n,0);
+        vector<int>left_max(n);
         left_max[0]=height[0];
 
-        vector<int>right_max(n,0);
+        vector<int>right_max(n);
         right_max[n-1]=height[n-1];
 
         for(int i=1;i<n;i++){
@@ -17,7 +17,7 @@ public:
         for(int i=n-2;i>=0;i--){
             right_max[i]=max(right_max[i+1],height[i]);
         }  
-        
+
         for(int i=0;i<n;i++){
             water_trapped+=min(left_max[i],right_max[i])-height[i];
         }  
