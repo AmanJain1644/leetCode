7bdb0626e0f7e2aef9f3214fbe8af9ad8a74b9d8
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-04-04 05:27:35 +0530

    Time: 129 ms (8.13%) | Memory: 376.2 MB (5.08%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
-        // approach -1 two pointers
-        int i=0;
-        int j=i+1;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        set<pair<int,int>>ans;
-        while(j<n){
-            if(nums[j]-nums[i]==k){
-                ans.insert({nums[i],nums[j]});
-                i++;j++;
+    bool search(int st,int x,vector<int> nums){
+        int ed = nums.size()-1;
+        while(st<=ed){
+            int mid = st+(ed-st)/2;
+            if(nums[mid]==x){
+                return true;
             }
-
-            else if(nums[j]-nums[i]>k){
-                i++;
+            else if(nums[mid]>x){
+                ed = mid-1;
             }else{
-                j++;
+                st=mid+1;
             }
 
-            if(i==j)j++;
-        }   
+        }
+        return false;
+    }
+    int findPairs(vector<int>& nums, int k) {
+        //approach -2 binary search
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        set<pair<int,int>>ans;
+        for(int i=0;i<n;i++){
+            if(search(i+1,nums[i]+k,nums)){
+                ans.insert({nums[i],nums[i]+k});
+            }           
+        }
+        return ans.size() ;
 
-        return ans.size()  ;
     }
 };
