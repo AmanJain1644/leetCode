8f0be1485ea9099665907bb91133926f896b60c1
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-04-15 14:11:27 +0530

    Time: 9 ms (22.85%) | Memory: 9 MB (30.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,13 +4,11 @@ public:
         string str="";
         int n = s.length();
         for(int i=0;i<n;i++){
-            if((s[i]>=97 && s[i]<=122)||(s[i]>=48 && s[i]<=57)){
-                str+=s[i];
-            }else if(s[i]>=65 && s[i]<=90){
-                str+=(s[i]-'A'+'a');
+            if(isalnum(s[i])){
+                str.push_back(tolower(s[i]));
             }
         }
-        cout<<str;
+        // cout<<str;
         int l = str.length();
         if(l==0 || l==1)return true;
         for(int i=0;i<(l/2);i++){
