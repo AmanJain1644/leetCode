e1b670e57e55764bd40aa560f0e4be4bcff282d1
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-04-15 14:13:04 +0530

    Time: 6 ms (46.21%) | Memory: 9.1 MB (21.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -10,7 +10,7 @@ public:
         }
         // cout<<str;
         int l = str.length();
-        if(l==0 || l==1)return true;
+        if(l<=1)return true;
         for(int i=0;i<(l/2);i++){
             if(str[i]!=str[l-1-i]){
                 return false;
