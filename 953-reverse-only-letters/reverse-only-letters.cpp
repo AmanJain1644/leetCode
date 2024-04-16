04cdfb94140b93fcdbefbab8405b512e89e8d8ad
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-04-16 21:27:14 +0530

    Time: 3 ms (43.84%) | Memory: 7.1 MB (92.75%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i =0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(!((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122))){
+                i++;
+            }else if(!((s[j]>=65 && s[j]<=90) || (s[j]>=97 && s[j]<=122))){
+                j--;
+            }else{
+                char temp = s[i];
+                s[i]=s[j];
+                s[j]=temp;
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
