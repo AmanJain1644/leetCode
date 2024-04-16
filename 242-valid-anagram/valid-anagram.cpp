9344d299a82cb091348d12d9da84fd96e99cca7c
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-04-16 19:30:07 +0530

    Time: 12 ms (30.09%) | Memory: 9.7 MB (32.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,11 +4,21 @@ public:
 
         if(s.length()!=t.length()){
             return false;
-        }  
-
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t; 
+        }
+        unordered_map<char,int>hashs;
+        unordered_map<char,int>hasht;
+        for(char val : s){
+            hashs[val]++;
+        }
+        for(char val:t){
+            hasht[val]++;
+        }
+        for(char val:t){
+            if(hashs[val]!=hasht[val]){
+                return false;
+            }
+        }
+        return true;
         
     }
 };
