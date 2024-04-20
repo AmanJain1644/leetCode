4c25c261112224f06cf967022c04ff34839fbbf7
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-04-20 23:04:03 +0530

    Time: 87 ms (94.91%) | Memory: 59.2 MB (91.64%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    vector<int> static checker(vector<vector<int>>& land,int i,int j,int& lx,int& ly,int& mx,int& my){
-        if(i<0 || j<0 || i>=land.size() || j>=land[0].size()){
-            return vector<int>{lx,ly,mx,my};
-        }
-        if(land[i][j]==0){
-            return vector<int>{lx,ly,mx,my};
-        }
-        land[i][j]=0;
-        lx=min(i,lx);
-        ly=min(j,ly);
-        mx=max(i,mx);
-        my=max(j,my);
-        vector<int>result= checker(land,i-1,j,lx,ly,mx,my);
-        result = checker(land,i+1,j,lx,ly,mx,my);
-        result = checker(land,i,j-1,lx,ly,mx,my);
-        result = checker(land,i,j+1,lx,ly,mx,my);
-        return result;
-
-
-    }
+    
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         vector<vector<int>>ans;
         int rows = land.size();
         int cols = land[0].size();
-        int count = 0;
         vector<int>cord ={-1};
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(land[i][j]){
-                    int lx=i,mx=i;
-                    int ly=j,my=j;
-                    ans.push_back(checker(land,i,j,lx,ly,mx,my));
-                    count++;
+                    int x = i; int y = j;
+                    for(x=i;x<rows && land[x][j]; x++){
+                        for(y=j;y<cols && land[x][y]; y++){
+                            land[x][y]=0;
+                        }
+                    }
+                    ans.push_back({i,j,x-1,y-1});                    
                 }
             }
         } 
