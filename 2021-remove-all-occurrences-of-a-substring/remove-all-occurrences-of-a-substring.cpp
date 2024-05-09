ce09133581534797ead7ecd0a915b63c9368255b
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-05-09 08:10:29 +0530

    Time: 7 ms (7.24%) | Memory: 11.9 MB (9.78%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
+    void removeocc(string&s,string&part){
+        int match = s.find(part);
+        if(match != string::npos){
+            string left = s.substr(0,match);
+            string right = s.substr(match+part.length(),s.length());
+            s=left+right;
+            removeocc(s,part);
+        }else{
+            return ;
+        }
+    }
     string removeOccurrences(string s, string part) {
-        int pos = s.find(part);
-        while(pos != string::npos){
-            s.erase(pos,part.length());
-            pos=s.find(part);
-        }   
-        return s;     
+        removeocc(s,part);
+        return s;        
     }
 };
