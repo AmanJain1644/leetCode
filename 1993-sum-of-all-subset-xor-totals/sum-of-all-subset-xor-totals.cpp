4d52b15821a64b58d8d5af91577c485de15baeda
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-05-21 06:09:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (65.21%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    void helper(vector<int>& nums,int i,vector<int>&current_subset,vector<vector<int>>&subsets){
+    int helper(vector<int>& nums,int i,int sum){
         if(i>=nums.size()){
-            subsets.push_back(current_subset);
-            return;
+            return sum;
         }
-        current_subset.push_back(nums[i]);
-        helper(nums,i+1,current_subset,subsets);
-        current_subset.pop_back();
-        helper(nums,i+1,current_subset,subsets);
+       
+        int include = helper(nums,i+1,nums[i]^sum);
+        
+       int exclude = helper(nums,i+1,sum);
+
+       return include+exclude;
         
     }
     int subsetXORSum(vector<int>& nums) {
-        vector<vector<int>>subsets;
-        vector<int>current_subset;
-        int sum = 0;
-        helper(nums,0,current_subset,subsets);
-        for(int i=0;i<subsets.size();i++){
-            int x = 0;
-            for(int j=0;j<subsets[i].size();j++){
-                x^=subsets[i][j];
-            }
-            sum+=x;
-            
-        }      
-    return sum;
+
+        return helper(nums,0,0);
+        
     }
 };
