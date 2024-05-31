030685a86f5246dfae86ee7ac69e964c1eaefc79
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-05-31 18:50:22 +0530

    Time: 4 ms (49.00%) | Memory: 15.1 MB (54.61%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(int n,int open,int closed,vector<string>&res,string curr){
+        if(closed==n){
+            res.push_back(curr);
+        }
+        if(open<n){
+            helper(n,open+1,closed,res,curr+'(');
+        }
+        if(open>closed){
+            helper(n,open,closed+1,res,curr+')');
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        helper(n,0,0,res,"");   
+        return res;     
+    }
+};
