e70e84aa804aa2885a459288f31d18bbfbc3af07
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-06-02 17:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (68.72%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    void addstr(string& num1,int p1,string& num2,int p2,string &ans,int carry=0){
+    void helper(string& num1,string& num2,int p1,int p2,int carry ,string& ans){
+
         if(p1<0 && p2<0){
+            
             if(carry){
-                ans+=carry+'0';
+            ans+=carry+'0';
+            return;
+            }else {
+                return ;
             }
-            return;       
+                
         }
-        if(p1>=0 && p2>=0){
-            int sum = num1[p1]-'0'+num2[p2]-'0'+carry;
-            ans+=(sum%10)+'0';
-            carry=sum/10;
-        }
-        else if(p1>=0){
-            int sum = num1[p1]-'0'+carry;
-            ans+=(sum%10)+'0';
-            carry=sum/10;
-        }
-        else if(p2>=0){
-            int sum = num2[p2]-'0'+carry;
-            ans+=(sum%10)+'0';
-            carry=sum/10;
-        }
-
-        addstr(num1,p1-1,num2,p2-1,ans,carry);
         
-
+        int n1 =(  p1>=0 ? num1[p1]:'0')-'0';
+        int n2 =(  p2>=0 ? num2[p2]:'0')-'0';
+        int sum = n1+n2+carry;
+        int digit = (sum)%10;
+        carry  =  (sum)/10;
+        ans+=digit+'0';
+        helper(num1,num2,p1-1,p2-1,carry,ans);
 
     }
     string addStrings(string num1, string num2) {
+        int p1 = 0;
+        int p2 = 0;
         string ans = "";
-        addstr(num1,num1.size()-1,num2,num2.size()-1,ans);
+        helper(num1,num2,num1.size()-1,num2.size()-1,0,ans);
         reverse(ans.begin(),ans.end());
-        return ans;                
+        return ans;
     }
 };
