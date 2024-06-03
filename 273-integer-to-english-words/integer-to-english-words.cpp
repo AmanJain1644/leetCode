5d0683d5f61d011b35e12a95206a55854b528325
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-06-03 11:26:03 +0530

    Time: 4 ms (51.49%) | Memory: 12.1 MB (36.09%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -6,29 +6,23 @@ public:
         {11,"Eleven"},{10,"Ten"},{9,"Nine"},{8,"Eight"},{7,"Seven"},{6,"Six"},{5,"Five"},{4,"Four"},{3,"Three"},{2,"Two"},{1,"One"}
     };
     string numberToWords(int num) {
-        
-        if(num==0){
-            return "Zero";
+        if(num==0) return "Zero" ;
+
+        for(auto it : word){
+            if(num>=it.first){
+                string a = "";
+                if(it.first>=100)
+                    a=numberToWords(num/it.first)+" ";
+                string b=it.second;
+                string c ="";
+            
+                if(num%it.first!=0){
+                    c = " "+numberToWords(num%it.first);
+                }
+                return a+b+c;
+            }
+            
         }
-        for(auto it:word){
-            if(num>=it.first){  
-                string a = ""; 
-            if(it.first>=100){
-                a=numberToWords(num/it.first);
-            }        
-            string b = it.second;
-            string c ="";
-            if(num%it.first!=0){
-                c = numberToWords(num%it.first);
-            }
-            if(a!=""){
-                a+=" ";
-            }if(c!=""){
-                b+=" ";
-            }
-            return a+b+c;
-            }
-        }   
         return "";
     }
 };
