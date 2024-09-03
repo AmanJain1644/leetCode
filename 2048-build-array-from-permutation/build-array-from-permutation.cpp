657deee175a532fc113049dcf05705eef00edf81
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-09-03 16:02:06 +0530

    Time: 11 ms (44.81%) | Memory: 19.2 MB (43.83%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
         vector<int>ans(nums.size(),0);
         for(int i=0;i<nums.size();i++){
             ans[i]=nums[nums[i]];
