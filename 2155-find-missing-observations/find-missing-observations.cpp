09244f42368d504a029bf970e8e909d8b9e4894b
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-09-06 00:55:47 +0530

    Time: 82 ms (93.14%) | Memory: 126.5 MB (8.58%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -34,3 +34,10 @@ public:
         return ans;                        
     }
 };
+
+static const int crown = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
