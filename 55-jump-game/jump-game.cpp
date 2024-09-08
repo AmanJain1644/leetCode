a6bd35a2d0d301d7dcf665ff4e5594fef3dc05c7
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-09-08 23:05:04 +0530

    Time: 36 ms (88.66%) | Memory: 51.2 MB (42.20%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int goal=n-1;
+        int n = nums.size();
+        int goal = n-1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]+i>=goal){
                 goal=i;
             }
-        }
-        return goal==0? true:false;        
+        }        
+        return goal==0?true:false;
     }
 };
