3894f74a81fa76b6bc4c2f119e9a6358a3763973
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-09-09 06:55:03 +0530

    Time: 721 ms (5.03%) | Memory: 77.1 MB (58.31%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long sum=0;
+        for(int i:chalk){
+            sum+=i;
+        }
+        while(true){
+            if(sum<=k){
+                k-=sum;
+            }else{
+                for(int i=0;i<chalk.size();i++){
+                    if(chalk[i]<=k && k>0){
+                        k-=chalk[i];
+                    }else{
+                        return i;
+                    }
+                }
+            }
+        }
+        
+        return 0;        
+    }
+};
