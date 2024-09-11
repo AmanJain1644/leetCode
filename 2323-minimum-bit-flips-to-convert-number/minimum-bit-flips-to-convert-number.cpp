d0fe326d8d82c991931d03cb11de58204e006fbf
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-09-12 02:43:17 +0530

    Time: 1 ms (42.39%) | Memory: 7.4 MB (93.45%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+        while(start || goal){
+            int s = start&1;
+            int g = goal&1;
+            
+            if(s^g){
+                count++;
+            }
+            start>>=1;
+            goal>>=1;
+
+        }  
+        
+        return count;                      
+    }
+
+};
