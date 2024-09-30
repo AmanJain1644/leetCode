e28201a7ef9d65fa48a62cdf93a18ea9f03f48e3
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-09-30 12:35:46 +0530

    Time: 35 ms (5.98%) | Memory: 12 MB (43.59%) - LeetSync

---
+++ 1260-day-of-the-year/day-of-the-year.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isLeapYear(int num){
+        cout<<num<<endl;
+        if(num%400==0) return true;
+        if(num%100==0) return false;
+        if(num%4==0) return true;
+        return false;
+    }
+    int dayOfYear(string date) {
+        int dates[3];
+        stringstream ss(date);
+        int i = 0;
+        string num;
+        while(getline(ss,num,'-')){
+            dates[i]=stoi(num);
+            i++;
+        }  
+        
+        int ans = dates[2];
+        int Month_days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        if(dates[1]>2){
+            bool isLeap = isLeapYear(dates[0]);
+            cout<<isLeap<<endl;
+            if(isLeap){
+                Month_days[1]=29;
+            }
+        }
+
+        for(int i=0;i<dates[1]-1;i++){
+            ans+=Month_days[i];
+            cout<<ans<<endl;
+        }
+        return ans;
+
+
+    }
+};
