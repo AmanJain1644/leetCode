75c98330c10a2d00f59890105802db76aec6e27e
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-02 12:27:59 +0530

    Time: 109 ms (19.31%) | Memory: 43.6 MB (47.33%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int>ans(arr.size(),0);
+        vector<int>rankingArr=arr;
+        map<int,int>hash;
+        sort(rankingArr.begin(),rankingArr.end());
+        int rank =1;
+        for(int i=0;i<arr.size();i++){
+            if(hash.find(rankingArr[i])==hash.end()){
+                hash[rankingArr[i]]=rank;
+                rank++;
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            ans[i]=hash[arr[i]];
+        }
+        return ans;
+
+
+    }
+};
