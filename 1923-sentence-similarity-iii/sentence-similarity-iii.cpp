f5eb0d30d2484e7e98b52163f28f01d999c78e1b
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-06 15:40:18 +0530

    Time: 4 ms (40.09%) | Memory: 8.6 MB (47.93%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        if(sentence1 == sentence2) return true;
+        stringstream ss1(sentence1);
+        string word;
+        stringstream ss2(sentence2);
+        vector<string>s1;
+        vector<string>s2;
+
+        while(ss1 >> word){
+            s1.push_back(word);
+        }
+
+        while(ss2 >> word){
+            s2.push_back(word);
+        }
+
+        int minLength = s1.size();
+        if(minLength>s2.size()){
+            minLength=s2.size();
+        }
+        int i=0;
+        int count = 0;
+        while(i<s1.size() && i<s2.size()){
+            if(s1[i]==s2[i]){
+                count++;
+            }else{
+                break;
+            }
+            i++;
+        }
+        i=s1.size()-1;
+        int j=s2.size()-1;
+        
+        while(i>=0 && j>=0){
+            if(s1[i]==s2[j]){
+                count++;
+            }else{
+                break;
+            }
+            i--; j--;
+        }
+        
+        if(count>=minLength) return true;
+
+        return false;
+
+
+        
+        
+    }
+};
