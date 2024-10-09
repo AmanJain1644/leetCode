605500a151fdb8fba2a807ddc795f944260ab3df
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-09 10:24:16 +0530

    Time: 31 ms (53.85%) | Memory: 8.3 MB (97.44%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.go
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.go
@@ -1,14 +1,16 @@
 func minSwaps(s string) int {
-    arr:= []rune{}
+    // arr:= []rune{}
     Imbalance:=0
     for _,c:=range s{
         if c=='['{
-            arr=append(arr,c)
-        }else if len(arr)>0{
-            arr = arr[:len(arr)-1]
-        }else{
-            Imbalance++;
-        }
+            // arr=append(arr,c)
+            Imbalance++
+        }else if Imbalance>0{
+            // arr = arr[:len(arr)-1]
+            Imbalance--
+        }// else{
+        //     Imbalance++
+        // }
     }
     return (Imbalance+1)/2;
 }
