ac43fa5c55183628c3e37825417f397c6eda3b09
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-09 10:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 2.2 MB (90.68%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.go
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.go
@@ -1,14 +1,19 @@
 func minAddToMakeValid(s string) int {
-    arr:=[]rune{}
+    // arr:=[]rune{}
+    OpenCount:=0
     Imbalance:=0
     for _,c:= range s{
         if c=='('{
-            arr=append(arr,c)
-        }else if len(arr)>0{
-            arr=arr[:len(arr)-1]
+            // arr=append(arr,c)
+            OpenCount++
+        // }else if len(arr)>0{
+        }else if OpenCount>0{
+            // arr=arr[:len(arr)-1]
+            OpenCount--
         }else{
             Imbalance++
         }
     }
-    return len(arr)+Imbalance
+    // return len(arr)+Imbalance
+    return OpenCount+Imbalance
 }
