bd3109b2fc079db434e614c63263d2956f239ba4
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-13 23:55:29 +0530

    Time: 24 ms (99.82%) | Memory: 21.5 MB (61.57%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        vector<pair<int,int>>elements;
+        int rangeLeft=0,rangeRight=0;
+        int range = INT_MAX;
+        cout<<range;
+        int k = nums.size();
+        for(int i=0;i<k;i++){
+            for(int num:nums[i]){
+                elements.push_back({num,i});
+            }
+        } 
+
+        sort(elements.begin(),elements.end());  
+        vector<int>count(k,0);
+        int left = 0;
+        int uniqueList = 0;
+
+        for(int right=0;right<elements.size();right++){
+            int rightList = elements[right].second;
+            if(count[rightList]==0){
+                uniqueList++;
+            }
+            count[rightList]++;
+
+            while(uniqueList==k){
+                int rightValue = elements[right].first;
+                int leftValue = elements[left].first;
+
+                if(range>(rightValue-leftValue)){
+                    range=rightValue-leftValue;
+                    rangeLeft=leftValue;
+                    rangeRight=rightValue;
+                }      
+
+                int leftList = elements[left].second;
+                count[leftList]--;
+                if(count[leftList]==0){
+                    uniqueList--;
+                }   
+                left++;     
+            }
+        }
+
+        return {rangeLeft,rangeRight};                     
+    }
+};
