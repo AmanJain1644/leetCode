9c71d85221c8943db733bf183225958e8027d1a4
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-19 22:17:51 +0530

    Time: 49 ms (25.09%) | Memory: 58.6 MB (42.32%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void stringMaker(string &str,string &substr){
+        int n = substr.size();
+        for(int i=0;i<n/2;i++){
+            swap(substr[i],substr[n-1-i]);
+        }
+        for(int i=0;i<n;i++){
+            if(substr[i]=='1'){
+                substr[i]='0';
+            }else{
+                substr[i]='1';
+            }
+        }
+        str+="1"+substr;
+    }
+    char findKthBit(int n, int k) {
+        string str = "0";
+        if(n==1){
+            return str[0];
+        }
+        for(int i=1;i<n;i++){
+            string substr = str;
+            stringMaker(str,substr);
+        }   
+        
+        for(int i=0;i<str.size();i++){
+            if(i+1==k){
+                return str[i];
+            }
+        }
+        return str[0]; 
+    }
+};
+
