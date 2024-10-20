3736d8de205ba6c54342313c885271a0e0b9fc21
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-20 20:55:40 +0530

    Time: 8 ms (59.20%) | Memory: 10.7 MB (47.48%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+
+        stack<char>st;
+
+        for(char c:expression){
+            if(c==','){
+                continue;
+            }else{
+                st.push(c);
+            }
+
+            if(!st.empty()&&st.top()==')'){
+                st.pop();
+                vector<char>nums;
+                while(st.top()!='('){
+                    nums.push_back(st.top());
+                    st.pop();
+                }
+                st.pop();
+                char operation = st.top();
+                st.pop();
+                char result = 'f';
+                if(operation=='&'){
+                    result = 't';
+                    for(char c:nums){
+                        if(c=='f'){
+                            result = 'f';
+                            break;
+                        }
+                    }                    
+                }else if(operation == '|'){
+                    for(char c:nums){
+                        result='f';
+                        if(c=='t'){
+                            result='t';
+                            break;
+                        }
+                    }
+                }else if(operation == '!'){
+                    result = nums[0]=='t'?'f':'t';
+                }
+                st.push(result);
+
+            }
+
+        }
+
+        return st.top()=='t';
+    }
+};
