c0ecde2a0b182a92910e3e26c0cb501ef49efdd9
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-22 22:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (89.84%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void backtrack(vector<string>&keys,string &curr,string &digits,int indx ,vector<string>&ans){
-        if(indx == digits.size()){
-            ans.push_back(curr);
-            return ;
-        }
+    void backtrack(vector<string>&keys,vector<string>&ans,string&curr,int indx,string &digits){
 
+        if(indx==digits.size()){
+            ans.push_back(curr);
+            return;
+        }
         string letters = keys[digits[indx]-'0'];
-        for(char letter:letters){
-            curr.push_back(letter);
-            backtrack(keys,curr,digits,indx+1,ans);
+        for(char c:letters){
+            curr.push_back(c);
+            backtrack(keys,ans,curr,indx+1,digits);
             curr.pop_back();
         }
-
     }
     vector<string> letterCombinations(string digits) {
-        if(digits=="") return {};
-        vector<string> ans;
-        vector<string> keys = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if(digits.empty()) return {};
+        vector<string>keys = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         string curr;
-        backtrack(keys,curr,digits,0,ans);
-
-        return ans;
-
+        vector<string>ans;
+        backtrack(keys,ans,curr,0,digits);
+        return ans;        
     }
 };
