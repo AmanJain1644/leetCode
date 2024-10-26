b44ef5a15e0441a698c2106c76dfde833953bc24
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-10-26 08:52:34 +0530

    Time: 11 ms (43.52%) | Memory: 17 MB (55.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        int r=1,c=1;
-        for(int i=0;i<cols;i++){
-            if(matrix[0][i]==0){
-                r=0;
-                break;
-            }
-        }
-        for(int i=0;i<rows;i++){
-            if(matrix[i][0]==0){
-                c=0;
-                break;
-            }
-        }
-        
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<pair<int,int>> zeroCount;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
+                    zeroCount.push_back({i,j});
                 }
             }
-        }
+        }    
 
-
-        for(int i=1;i<cols;i++){
-            if(matrix[0][i]==0){
-                for(int j=1;j<rows;j++){
-                    matrix[j][i]=0;
+        for(pair point:zeroCount){
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    matrix[point.first][j]=0;
+                    matrix[i][point.second]=0;
                 }
             }
-        }
-
-        for(int i=1;i<rows;i++){
-            if(matrix[i][0]==0){
-                for(int j=1;j<cols;j++){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-
-
-
-        if(r==0){
-            for(int i=0;i<cols;i++){
-                matrix[0][i]=0;
-            }
-        }
-
-        if(c==0){
-            for(int i=0;i<rows;i++){
-                matrix[i][0]=0;
-            }
-        }
-        
-
+        }        
     }
 };
