ad746a5f1a4fbe861fce9c5ef3fdba154d63863b
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-11-03 08:51:49 +0530

    Time: 143 ms (5.00%) | Memory: 128.1 MB (5.45%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,14 +10,43 @@
  */
 class Solution {
 public:
+    void merge(vector<int>&nodes,int st,int mid,int ed){
+        vector<int>L = {nodes.begin()+st,nodes.begin()+mid+1};
+        vector<int>R = {nodes.begin()+mid+1,nodes.begin()+ed+1};
+        int i=0,j=0,k=st;
+        while(i<L.size() && j<R.size()){
+            if(L[i]<=R[j]){
+                nodes[k++]=L[i++];
+            }else{
+                nodes[k++]=R[j++];
+            }
+        }
+
+        while(i<L.size()){
+            nodes[k++]=L[i++];
+        }
+
+        while(j<R.size()){
+            nodes[k++]=R[j++];
+        }
+
+    }
+  void divide(vector<int>&nodes,int st,int ed){
+        if(ed-st+1 <=1 ) return;
+        int mid = st+(ed-st)/2;
+        divide(nodes,st,mid);
+        divide(nodes,mid+1,ed); 
+        merge(nodes,st,mid,ed); 
+    }
+
     ListNode* sortList(ListNode* head) {
         ListNode* x = head;
         vector<int>nodes;
         while(x){
             nodes.push_back(x->val);
             x=x->next;
-        }  
-        sort(nodes.begin(),nodes.end());
+        }
+        divide(nodes,0,nodes.size()-1);  
         x=head;
         for(auto node:nodes){
             x->val=node;
