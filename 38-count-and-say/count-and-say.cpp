73a837fec586bbcc58b403cd8da0dd701b840af3
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-11-06 00:11:51 +0530

    Time: 2 ms (90.93%) | Memory: 8.2 MB (96.99%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1)return"1";
-        string str="1";        
-        for(int i=2;i<n+1;i++){
-            string update="";
-            str+="$";
-            int count=1;
+        if(n==1) return "1";
+        string str = "1";
+        int count = 1;
+        
+        for(int i=1;i<n;i++){
+            string update ="";
+            str+='#';
             for(int j=1;j<str.size();j++){
-                if(str[j-1]!=str[j]){
+                if(str[j]!=str[j-1]){
                     update+=(count+'0');
                     update+=str[j-1];
-                    count=1;
+                    count = 1;
                 }else{
                     count++;
                 }
             }
-            str=update;
-        }
-        return str;
+            str = update;
+        }    
+        return str;    
     }
-        
-        
-    
 };
