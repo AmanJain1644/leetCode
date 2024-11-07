f7383322819359661aec9681cd7506a8c4733d71
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2024-11-08 00:11:44 +0530

    Time: 30 ms (62.69%) | Memory: 60.3 MB (32.98%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
         int max_count = 0;
-        for(int i=0;i<25;i++){
+        for(int i=0;i<24;i++){
             int count = 0;
             for(int num:candidates){
                 if(num &(1<<i)){
