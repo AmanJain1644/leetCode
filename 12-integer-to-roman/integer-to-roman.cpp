26bed15ebc1e54648cd82372a5d937c489e23ad3
Aman Jain <88729130+AmanJain1644@users.noreply.github.com>
2025-08-05 00:11:44 +0530

    Time: 16 ms (5.57%) | Memory: 14.5 MB (8.06%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,16 +1,55 @@
 class Solution {
 public:
+    std::unordered_map<int,std::string>ior_map = {{1,"I"},{5,"V"},{10,"X"},{50,"L"},{100,"C"},{500,"D"},{1000,"M"}};
+    std::string str = "";
+    int div = 1;
+
     string intToRoman(int num) {
-        string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        int val[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string ans="";        
-        for(int i=0;i<13;i++){
-            while(num>=val[i]){
-                ans+=roman[i];
-                num-=val[i];
-            }
+        if (num <1 || num>3999){
+            return "";
         }
-        
-        return ans;       
+        while(num && div <=1000){
+            if(ior_map.find(num*div)!=ior_map.end()){
+               return  str = ior_map[num*div] + str;
+            }
+            int val = num%10*div;
+            num = num/10;
+            if(val == 0){
+                div*=10;
+                continue;
+            }
+            if(ior_map.find(val)!=ior_map.end()){
+                str= ior_map[val] + str;
+            }else{
+                if(val == 9*div){
+                    str = ior_map[div]+ior_map[10*div] + str;
+                }
+                else if(val > 5*div){
+                    int count = 5*div;
+                   std::string temp_str = ior_map[5*div];
+                    while(val>count){
+                        temp_str = temp_str + ior_map[div];
+                        count += div;
+                    }
+                    str = temp_str + str;
+                }else{
+                    if(val == 4*div){
+                        str = ior_map[div] + ior_map[5*div] + str;
+                    }else{
+                        int count = div;
+                        std::string temp_str = ior_map[div];
+                        while(count < val ){
+                            temp_str += ior_map[div];
+                            count += div;
+                        }
+                        str = temp_str + str;
+                    }
+                }
+               
+            }
+            div*=10;
+
+        }       
+        return str; 
     }
 };
